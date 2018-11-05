daa50e4a7d503dd1308dfd518aedfc1be5287cf8
NajamShehzad <najambutt195@gmail.com>
2018-11-05 15:12:51 +0500

    Card From React Native Element

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 import Deck from './components/Deck/Deck';
 
-
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+];
 
 
 export default class App extends React.Component {
+
+
+  renderCard(item) {
+    return (
+      <Card
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text style={{ marginBottom: 10 }} >
+          Card Info
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now"
+        />
+      </Card>
+    )
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Deck />
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+        />
       </View>
     );
   }
--- components/Deck/Deck.js
+++ components/Deck/Deck.js
@@ -5,10 +5,17 @@ import { View, Animated } from 'react-native';
 
 
 class Deck extends Component {
+
+    renderCards() {
+        return this.props.data.map(item => {
+            return this.props.renderCard(item)
+        })
+    }
+
     render() {
         return (
             <View>
-
+                {this.renderCards()}
             </View>
         )
     }
