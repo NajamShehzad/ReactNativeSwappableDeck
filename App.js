e4fdb4e732cc3cf253f045ee4b380df488569c2f
NajamShehzad <najambutt195@gmail.com>
2018-11-12 05:37:56 +0500

    Notitfaction updated

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Notifications, Permissions } from 'expo';
+// import firebase from 'firebase'
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 import Deck from './components/Deck/Deck';
+import firebaseKey from './firebaseApi';
+
 
 const DATA = [
   { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
@@ -36,6 +40,47 @@ export default class App extends React.Component {
       </Card>
     )
   }
+  componentWillMount() {
+    // firebase.initializeApp(firebaseKey);
+
+  }
+  componentDidMount() {
+
+    this.registerPushNotifications();
+
+  }
+  registerPushNotifications = async () => {
+
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    console.log(finalStatus);
+
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+      return;
+    }
+
+    // Get the token that uniquely identifies this device
+    Notifications.getExpoPushTokenAsync().then(x => {
+      console.log(x);
+
+    }).catch(err => {
+      console.log(err);
+    });
+
+
+  }
 
   renderNoMoreCards() {
     return (
@@ -55,20 +100,20 @@ export default class App extends React.Component {
   render() {
     return (
       // <ScrollView>
-        <View style={styles.container}>
-          <Deck
-            data={DATA}
-            renderCard={this.renderCard}
-            onSwipeLeft={() => console.log("Left Working")}
-            onSwipeRight={() => console.log('Right Working')}
-            renderNoMoreCards={this.renderNoMoreCards}
-          />
-          <View>
-            <Text>
-              Test Under images
+      <View style={styles.container}>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+          onSwipeLeft={() => console.log("Left Working")}
+          onSwipeRight={() => console.log('Right Working')}
+          renderNoMoreCards={this.renderNoMoreCards}
+        />
+        <View>
+          <Text>
+            Test Under images
             </Text>
-          </View>
         </View>
+      </View>
       // </ScrollView>
     );
   }
